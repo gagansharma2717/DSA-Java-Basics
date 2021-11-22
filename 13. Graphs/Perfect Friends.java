b6624fc9da29ed6b200dfbf84ab42f8bd98410f2
Gagan Sharma <gagansharma2717@gmail.com>
2021-11-23 00:07:21 +0530

    Update Perfect Friends.java

--- 13. Graphs/Perfect Friends.java
+++ 13. Graphs/Perfect Friends.java
@@ -2,72 +2,95 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   static class Edge {
-      int src;
-      int nbr;
-      int wt;
-
-      Edge(int src, int nbr, int wt) {
-         this.src = src;
-         this.nbr = nbr;
-         this.wt = wt;
-      }
-   }
    
-   public static void dfs(ArrayList<Edge>[] graph, int[] count, int src, boolean[] vis){
-        vis[src] = true;
-        count[0]++;
-        
-        for(Edge e: graph[src]){
-            if(vis[e.nbr] == false){ 
-                dfs(graph, count, e.nbr, vis);
-            }
+   public static class Edge{
+       int vertex;
+       int nbr;
+       
+        Edge(int vertex,int nbr)
+        {
+            this.vertex = vertex;
+            this.nbr = nbr ;
         }
    }
-   
+
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      int vtces = Integer.parseInt(br.readLine());
-      ArrayList<Edge>[] graph = new ArrayList[vtces];
-      for (int i = 0; i < vtces; i++) {
-         graph[i] = new ArrayList<>();
-      }
-
-      int edges = Integer.parseInt(br.readLine());
-      for (int i = 0; i < edges; i++) {
-         String[] parts = br.readLine().split(" ");
-         int v1 = Integer.parseInt(parts[0]);
-         int v2 = Integer.parseInt(parts[1]);
-         graph[v1].add(new Edge(v1, v2, -1));
-         graph[v2].add(new Edge(v2, v1, -1));
-      }
-
-      ArrayList<Integer> comps = new ArrayList<>();
-      boolean[] vis = new boolean[vtces];
+      int n = Integer.parseInt(br.readLine());
+      int k = Integer.parseInt(br.readLine());
       
-      for(int i=0; i<vtces; i++){
-          if(vis[i] == false){
-              int[] count = new int[1];
-              dfs(graph, count, i,vis);
-              comps.add(count[0]);
+      // write your code here
+      //Array created of blank arraylist 
+      ArrayList<Edge>[] graph = new ArrayList[n];
+      for(int v = 0 ; v < n ; v++)
+      {
+          graph[v] = new ArrayList<>();
+      }
+      
+      //Implementing Adjacency list
+      for(int i = 0 ; i < k ; i++)
+      {
+          // reading a whole line
+          String line = br.readLine();
+          // spliting that line on basis of space and storing it in string array
+          String[] parts = line.split(" ");
+          
+          //adding value of parrs[0] in v1 and parts[1] in  v2
+          int v1 = Integer.parseInt(parts[0]);
+          int v2 = Integer.parseInt(parts[1]);
+          
+          //adding edges in arraylist
+          
+          graph[v1].add(new Edge(v1,v2));
+          graph[v2].add(new Edge(v2,v1));
+      }
+      
+      boolean[] visited = new boolean[n];
+      ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+      for(int j = 0 ; j < n; j++)
+      {
+          if(visited[j] == false)
+          {
+            ArrayList<Integer> comp = new ArrayList<>();
+            dfs(graph,j,comp,visited);
+            comps.add(comp);
           }
       }
       
-    //   int countWays = 0;
-    //   for(int c1 = 0; c1 < comps.size(); c1++){
-    //       for(int c2 = c1 + 1; c2 < comps.size(); c2++){
-    //           countWays = countWays + comps.get(c1) * comps.get(c2);
-    //       }
-    //   }
-    
-      int countWays = 0;
-      int remVtces = vtces;
+      int pairs = 0;
       
-      for(int size: comps){
-          remVtces -= size;
-          countWays += remVtces * size;
+      //Components arraylist pr loop bcoz usme components honge and hm unka size multiply krke aapna answer bna lenge
+      for(int i = 0 ; i < comps.size(); i++)
+      {
+          for(int j = i  + 1 ; j < comps.size(); j++)
+          {
+              int count = comps.get(i).size() * comps.get(j).size();
+              
+              pairs += count;
+          }
       }
-      System.out.println(countWays);
+      
+      System.out.println(pairs);
+     
    }
+   
+   public static void dfs(ArrayList<Edge>[] graph, int src , ArrayList<Integer> comp, boolean[] visited)
+   {
+       
+       visited[src] = true;
+       comp.add(src);
+       
+       for(Edge e : graph[src])
+       {
+           if(visited[e.nbr] == false)
+           {
+            
+                 dfs(graph,e.nbr,comp,visited);
+           }
+       }
+       
+       
+   }
+
 }
