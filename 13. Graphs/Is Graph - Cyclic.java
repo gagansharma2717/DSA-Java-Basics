b3f6c67ab0187ed170642f0f4f2f1ff4a7f14c30
Gagan Sharma <gagansharma2717@gmail.com>
2021-11-23 00:09:15 +0530

    Update Is Graph - Cyclic.java

--- 13. Graphs/Is Graph - Cyclic.java
+++ 13. Graphs/Is Graph - Cyclic.java
@@ -1,4 +1,3 @@
-// Undirected Graph - BFS - Variation 1
 import java.io.*;
 import java.util.*;
 
@@ -6,46 +5,26 @@ public class Main {
    static class Edge {
       int src;
       int nbr;
+      int wt;
 
-      Edge(int src, int nbr) {
+      Edge(int src, int nbr, int wt) {
          this.src = src;
          this.nbr = nbr;
+         this.wt = wt;
       }
    }
-
-   static class Pair{
-       int node;
-       String pathSofar;
-       
-       Pair(int node, String pathSofar){
-           this.node = node;
-           this.pathSofar = pathSofar;
-       }
-   }
-   
-   public static boolean BFS(ArrayList<Edge>[] graph, boolean[] vis, int src){
-      Queue<Pair> q = new LinkedList<>();
-      q.add(new Pair(src, "" + src));
-      
-      while(q.size() > 0){
-          // remove
-          Pair curr = q.remove();
-          
-          if(vis[curr.node] == true) return true;
-          
-          // mark*
-          vis[curr.node] = true;
-          
-          // add*
-          for(Edge e: graph[curr.node]){
-              if(vis[e.nbr] == false){
-                  q.add(new Pair(e.nbr, curr.pathSofar + e.nbr));
-              }
-          }
-      }
-      
-      return false;
-   }
+    
+    static class Pair
+    {
+        int v; 
+        String psf;
+        
+        Pair(int v, String psf)
+        {
+            this.v = v;
+            this.psf = psf;
+        }
+    }
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -60,21 +39,61 @@ public class Main {
          String[] parts = br.readLine().split(" ");
          int v1 = Integer.parseInt(parts[0]);
          int v2 = Integer.parseInt(parts[1]);
-         graph[v1].add(new Edge(v1, v2));
-         graph[v2].add(new Edge(v2, v1));
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
       }
+
+      // write your code here
       
-      boolean[] vis = new boolean[vtces];
-      for(int i=0; i<vtces; i++){
-          if(vis[i] == false){
-              // new Component
-              boolean isCycle = BFS(graph, vis, i);
-              if(isCycle == true) {
-                  System.out.println(true);
+      boolean[] visited = new boolean[vtces];
+      
+      for(int v = 0 ; v < vtces; v++)
+      {
+          if(visited[v] == false)
+          {
+              //traversal
+              boolean cycle = bfs(graph,v,visited);
+              
+              if(cycle)
+              {
+                  System.out.println("true");
                   return;
               }
+              
           }
       }
-      System.out.println(false);
+      
+      System.out.println("false");
+   }
+   
+   public static boolean bfs(ArrayList<Edge>[] graph, int src , boolean[] visited)
+   {
+       ArrayDeque<Pair> queue = new ArrayDeque<>();
+       queue.add(new Pair(src,src+""));
+       
+       while(queue.size() > 0)
+       {
+           // r m* w a*
+           
+           Pair rem = queue.removeFirst();
+           
+           if(visited[rem.v] == true)
+           {
+               return true;
+           }
+           visited[rem.v] = true;
+           
+           
+           for(Edge e : graph[rem.v])
+           {
+               if(visited[e.nbr] == false)
+               {
+                   queue.add(new Pair(e.nbr,rem.psf+e.nbr));
+                }
+           }
+       }
+       
+       return false;
    }
 }
