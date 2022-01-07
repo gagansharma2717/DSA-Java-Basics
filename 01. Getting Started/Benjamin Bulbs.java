830cc597b80e747e884af3b963a2286ecb76de56
Gagan Sharma <gagansharma2717@gmail.com>
2022-01-07 18:52:56 +0530

    Update Benjamin Bulbs.java

--- 01. Getting Started/Benjamin Bulbs.java
+++ 01. Getting Started/Benjamin Bulbs.java
@@ -3,10 +3,12 @@ import java.util.*;
   public class Main{
   
   public static void main(String[] args) {
+    // write your code here  
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
+    int num = scn.nextInt();
     
-    for(int i = 1; i * i <= n; i++){
+    for(int i = 1 ; i * i < num ; i++)
+    {
         System.out.println(i*i);
     }
    }
